3002f2a6ee6a44f3daa799a6b89112d3ccce5201
Maximilian Mensing <max@maximilian-mensing.de>
2018-08-08 01:01:58 +0200

    added wrapper

--- swipl_kernel.py
+++ swipl_kernel.py
@@ -27,6 +27,42 @@ def exec_swipl(code):
     return getoutput(target_path)
     
 
+"""SWI-Prolog kernel wrapper"""
+from ipykernel.kernelbase import Kernel
+
+class SwiplKernel(Kernel):
+
+    # Kernel information.
+    implementation = 'SWI-Prolog'
+    implementation_version = '0.0'
+    language = 'Prolog'
+    language_version = '1.0'
+    language_info = {'name': 'swipl',
+                     'mimetype': 'text/plain'}
+    banner = "SWI-Prolog Kernel"
+
+    def do_execute(self, code, silent,
+                   store_history=True,
+                   user_expressions=None,
+                   allow_stdin=False):
+        """This function is called when a code cell is
+        executed."""
+        if not silent:
+            # We run the Prolog code and get the output.
+            output = exec_swipl(code)
+
+            # We send back the result to the frontend.
+            stream_content = {'name': 'stdout',
+                              'text': output}
+            self.send_response(self.iopub_socket,
+                              'stream', stream_content)
+        return {'status': 'ok',
+                # The base class increments the execution
+                # count
+                'execution_count': self.execution_count,
+                'payload': [],
+                'user_expressions': {},
+               }
 
 if __name__ == '__main__':
     from ipykernel.kernelapp import IPKernelApp
