6005dc551fd62c06b200df497fb5009b6cc94c43
Maximilian Mensing <max@maximilian-mensing.de>
2018-10-03 09:53:17 +0200

    added martins getall function, tempfiles, first streamlining

--- swipl_kernel.py
+++ swipl_kernel.py
@@ -6,22 +6,49 @@ import jupyter_client
 from IPython.utils.process import getoutput
 
 def exec_swipl(code):
-    source_path, target_path = setup_env()
-
-    with open(source_path, 'w') as f:
+    temp_path, output_path, code_path = setup_env()
+    dirpath = '/home/max/.local/share/jupyter/kernels/swi/temp'  # tempfile.mkdtemp()
+    preCode = 'do([]). do([V-G|R]) :-  findall(V, ( call(G),  write(G), nl), _), do(R).'
+    startQuery = ':- do(['
+    endQuery = ']).'
+    with open(temp_path, 'w') as f: # 
         f.write(code)
+    ''' Parser code begins here '''
+    started = 0
+    textlist = []
+    textlist.append(preCode)
+    with open(temp_path) as f: 
+        ''' 
+        some hacky logic to enable students to simply add queries
+        after defining their ruleset. 
+        '''
+        for line in f:
+            textlist.append(line)
+            if 'QUERYSTART' in line:
+                textlist.pop() # get rid of start marker
+                textlist.append(startQuery) 
+            if 'QUERYEND' in line:
+                textlist.pop() # get rid of end marker
+                textlist.append(endQuery) 
+    print(''.join(textlist))
+    with open(code_path, 'w') as rules:
+        rules.write(''.join(textlist))
+    #rules_target_path
+    ''' Parser code begins here '''
 
-    os.system("swipl {0:s} > {1:s}".format(source_path, target_path))  # source_path, program_path))
+
+    os.system("swipl {0:s} > {1:s}  2>&1 ".format(code_path, output_path))  # source_path, program_path))
     #out = os.system("swipl {0:s} ".format(source_path))  # source_path, program_path))
-    f = open(target_path, 'r')
+    f = open(output_path, 'r')
     return f.read()
 
 def setup_env():
     dirpath = '/home/max/.local/share/jupyter/kernels/swi/temp'  # tempfile.mkdtemp()
 
-    source_path = op.join(dirpath, 'temp.pl')
-    target_path = op.join(dirpath, 'out.txt')
-    return source_path, target_path
+    temp_path = op.join(dirpath, 'temp.pl')
+    output_path = op.join(dirpath, 'out.txt')
+    code_path = op.join(dirpath, 'code.pl')
+    return temp_path, output_path, code_path 
 
 
     
